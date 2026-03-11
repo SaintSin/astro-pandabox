54203f3a833258bec0509120a98d6e8621d04abf
User <user@example.com>
2026-03-11 10:08:42 +0000

    upgrade to Astro 6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,13 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 
 // 2. Import loader(s)
 import { glob } from 'astro/loaders';
 
-// 3. Define your collection(s)
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+// 4. Define your collection(s)
 const galleries = defineCollection({
   loader: glob({ pattern: '*.json', base: 'src/content/galleries' }),
   schema: ({ image }) =>
